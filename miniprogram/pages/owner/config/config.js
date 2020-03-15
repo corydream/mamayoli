4043f61deb97fd5e9c825edc5adb59aa038aa4c0
corydream <446108913@qq.com>
2020-03-15 16:48:35 +0800

    提交测试

---
+++ miniprogram/pages/owner/config/config.js
@@ -0,0 +1,21 @@
+//index.js
+
+//index.js
+import creatorPage from '../../utils/create';
+import ConfigRecordService from './config.service';
+
+const app = getApp()
+class Index {
+  data = {
+    userData:{}
+  }
+  constructor() {
+    this.ser = new ConfigRecordService();
+  }
+  onLoad() {
+    
+  }
+
+  
+}
+Page(creatorPage(Index));
