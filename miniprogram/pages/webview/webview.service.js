1305b8e328137d3fa88ca64ddf2d044376a3b157
corydream <446108913@qq.com>
2020-04-15 19:24:53 +0800

    压缩图片

--- miniprogram/pages/webview/webview.service.js
+++ miniprogram/pages/webview/webview.service.js
@@ -1,17 +1,8 @@
 
 import request from '../../service/http';
 export default class WebViewService {
-    
-    login(param) {
-        return request().post('/user/login', param)
-    }
-    updateInfo(param) {
-        return request().post('/user/updateUserInfo', param)
-    }
-    getUserInfo(url) {
+
+    getTodo(url) {
         return request().get(url)
-    }
-    list(param) {
-        return request().get('/activity/list', param)
     }
 }
